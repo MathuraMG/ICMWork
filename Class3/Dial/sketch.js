2c32bf3784ba728a12ce773104c2e90a0b35d4b5
MathuraMG <mmg9291@gmail.com>
2015-09-22 15:34:18 -0400

    Update sketch.js

--- Class3/Dial/sketch.js
+++ Class3/Dial/sketch.js
@@ -1,3 +1,9 @@
+var mouseXclick = 200;
+var mouseXclick2 = 200;
+var mouseYclick = 30;
+var waveSpeed = 2; //lower value makes bigger wave height
+var waveFactor = 10; //waveSpeed
+
 var theta = 90;
 var theta1 = 90;
 
@@ -5,17 +11,22 @@ var xCtr;
 var yCtr;
 var lineColour1 = 100;
 var lineColour2 = 100;
+var dotSize = 5;
+var waveSpeed2 = 2;
+
+var direction = 1;
+var direction2 = 1;
 
 function setup() {
-  createCanvas(1000, 500);
-  background(255);
+  createCanvas(900, 500);
+  background;
+  stroke(255);
   angleMode(DEGREES);
 
 }
 
 function draw() {
-  background(255);
-  //x,y cordinates of the dial's center
+
   var xCtr = 250;
   var yCtr = 500;
   //length of the dial
@@ -23,19 +34,73 @@ function draw() {
   //values for the range to be displayed
   var minVal = 25;
   var maxVal = 700;
+  var t;
+  var r;
+
   //function to create the dial
-  dial1 = createDial(xCtr, yCtr, l, minVal, maxVal, theta,lineColour1);
+
+  dial1 = createDial(xCtr, yCtr, l, minVal, maxVal, theta, lineColour1);
   theta = dial1.theta;
   lineColour1 = dial1.lineColour;
-  //Vlue to be used for the amplitude 
-  amp = dial1.value;
-
-  dial2 = createDial(xCtr + 500, yCtr, l, minVal, maxVal, theta1,lineColour2);
+  t = dial1.value;
+  //r= createDial(xCtr+400, yCtr, l, minVal, maxVal);
+  dial2 = createDial(xCtr + 400, yCtr, l, 1, 5, theta1, lineColour2);
   theta1 = dial2.theta;
   lineColour2 = dial2.lineColour;
-  //Vlue to be used for the speed 
-  speed = dial2.value;
+  r = dial2.value;
+
+  dotSize = random(1, 10);
+  angleMode(RADIANS);
+  stroke(250);
+  strokeWeight(dotSize);
+  background(50, 10)
+  point(mouseXclick, mouseYclick);
+  point(mouseXclick2, mouseYclick);
+
+  mouseXclick = mouseXclick - waveSpeed;
+  mouseXclick2 = mouseXclick2 + waveSpeed2;
+
+  if (mouseXclick < 0 || mouseXclick > width) {
+    waveSpeed = waveSpeed * -1;
+    if (waveSpeed < 0)
+      direction = -1
+    else
+      direction = 1
+  } else {}
+
+  if (mouseXclick2 < 0 || mouseXclick2 > width) {
+    waveSpeed2 = waveSpeed2 * -1;
+    if (waveSpeed2 < 0)
+      direction2 = -1
+    else
+      direction2 = 1
+
+  } else {}
+
+  mouseYclick = mouseYclick + waveFactor * sin(mouseXclick);
+  //println(waveSpeed2);
+  waveFactor = t / 5;
+  waveSpeed = direction * r;
+  waveSpeed2 = direction2 * r;
+
+
+
 }
+
+function mousePressed() {
+  // background(50);
+  if (mouseY > 350) {
+    //println(mouseX);
+    //println(mouseY);
+    //println('----')
+  } else {
+    stroke(0);
+    mouseXclick = mouseX;
+    mouseXclick2 = mouseX;
+    mouseYclick = mouseY;
+  }
+}
+
 
 function createDial(x, y, l, minVal, maxVal, theta, lineColour) {
   //COLOUR THE DIAL
@@ -61,16 +126,14 @@ function createDial(x, y, l, minVal, maxVal, theta, lineColour) {
   if (mouseIsPressed && inRange(mouseX, mouseY, x + l * cos(180 - theta), y + l * sin(-theta), x, y, l)) {
     theta = tanInv((x - mouseX), (y - mouseY));
     lineColour = 10;
-  } 
-  
-  else
+  } else
     lineColour = 100;
-  
+
   var value;
   //Map the values to the position of the dial
   value = map(theta, 0, 180, minVal, maxVal);
-  text(value, 25, 25);
-  print(theta);
+
+  //print(theta);
   return {
     theta: theta,
     value: value,
@@ -86,8 +149,8 @@ function inRange(x, y, x1, y1, xCtr, yCtr, l) {
 
   //print(sqrt(pow((x1 - x), 2) + pow((y1 - y), 2)));
 
-  if ((abs(tanInv((xCtr - x1), (yCtr - y1)) - tanInv((xCtr - x), (yCtr - y))) < 20))
-  //&& (sqrt(pow((x - xCtr), 2) + pow((y - yCtr), 2)) < (l + 20)))
+  if ((sqrt(pow((x - xCtr), 2) + pow((y - yCtr), 2)) < (l + 20)))
+  // && (abs(tanInv((xCtr - x1), (yCtr - y1)) - tanInv((xCtr - x), (yCtr - y))) < 30) )
     return 1;
   else
     return 0;
