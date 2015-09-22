13219b16819bf5321c29368c1f27fd78c1a1085c
MathuraMG <mmg9291@gmail.com>
2015-09-21 23:58:11 -0400

    change code to add multiple dials

--- Class3/Dial/sketch.js
+++ Class3/Dial/sketch.js
@@ -1,12 +1,13 @@
 var theta = 90;
-var xclick;
-var yclick;
+var theta1 = 90;
+
 var xCtr;
 var yCtr;
-var LineColour = 100;
+var lineColour1 = 100;
+var lineColour2 = 100;
 
 function setup() {
-  createCanvas(500, 500);
+  createCanvas(1000, 500);
   background(255);
   angleMode(DEGREES);
 
@@ -18,52 +19,63 @@ function draw() {
   var xCtr = 250;
   var yCtr = 500;
   //length of the dial
-  var l = 150;
+  var l = 100;
   //values for the range to be displayed
   var minVal = 25;
   var maxVal = 700;
   //function to create the dial
-  createDial(xCtr, yCtr, l, minVal, maxVal);
+  dial1 = createDial(xCtr, yCtr, l, minVal, maxVal, theta,lineColour1);
+  theta = dial1.theta;
+  lineColour1 = dial1.lineColour;
+  //Vlue to be used for the amplitude 
+  amp = dial1.value;
 
-
+  dial2 = createDial(xCtr + 500, yCtr, l, minVal, maxVal, theta1,lineColour2);
+  theta1 = dial2.theta;
+  lineColour2 = dial2.lineColour;
+  //Vlue to be used for the speed 
+  speed = dial2.value;
 }
 
-function createDial(x, y, l, minVal, maxVal) {
-
+function createDial(x, y, l, minVal, maxVal, theta, lineColour) {
   //COLOUR THE DIAL
   fill(255, 50, 50);
   stroke(255);
+  //draw the dial
   for (var i = 8; i > 0; i--) {
     angleMode(DEGREES);
     stroke(25 * i);
     fill(25 * i);
-    arc(x, y, 50 * i, 50 * i, 180, 0, PIE);
+    arc(x, y, 30 * i, 30 * i, 180, 0, PIE);
     fill(25 * i - 45);
     stroke(25 * i - 45);
     angleMode(DEGREES);
-    arc(x, y, 50 * i, 50 * i, 180, 180 + theta, PIE);
-
-
+    arc(x, y, 30 * i, 30 * i, 180, 180 + theta, PIE);
   }
-
+  //draw the indicator
   push();
   translate(x, y);
-
-
-  drawDial(0, 0, l * cos(180 - theta), l * sin(-theta), LineColour);
+  drawNeedle(0, 0, l * cos(180 - theta), l * sin(-theta), lineColour);
   pop();
 
-  if (mouseIsPressed && inRange(mouseX, mouseY, xclick, yclick, x, y, l)) {
-    xclick = mouseX;
-    yclick = mouseY;
-    theta = tanInv((x - xclick), (y - yclick));
-    LineColour = 10;
-  } else
-    LineColour = 100;
+  if (mouseIsPressed && inRange(mouseX, mouseY, x + l * cos(180 - theta), y + l * sin(-theta), x, y, l)) {
+    theta = tanInv((x - mouseX), (y - mouseY));
+    lineColour = 10;
+  } 
+  
+  else
+    lineColour = 100;
+  
   var value;
   //Map the values to the position of the dial
   value = map(theta, 0, 180, minVal, maxVal);
   text(value, 25, 25);
+  print(theta);
+  return {
+    theta: theta,
+    value: value,
+    lineColour: lineColour
+  }
 
 }
 
@@ -72,13 +84,16 @@ function inRange(x, y, x1, y1, xCtr, yCtr, l) {
   if (!x1) x1 = xCtr;
   if (!y1) y1 = yCtr - l;
 
-  if (abs(tanInv((xCtr - x1), (yCtr - y1)) - tanInv((xCtr - x), (yCtr - y))) < 20)
+  //print(sqrt(pow((x1 - x), 2) + pow((y1 - y), 2)));
+
+  if ((abs(tanInv((xCtr - x1), (yCtr - y1)) - tanInv((xCtr - x), (yCtr - y))) < 20))
+  //&& (sqrt(pow((x - xCtr), 2) + pow((y - yCtr), 2)) < (l + 20)))
     return 1;
   else
     return 0;
 }
 
-function drawDial(startX, startY, endX, endY, LineColour)
+function drawNeedle(startX, startY, endX, endY, LineColour)
   //drawDial(0, 0, -l * cos(theta), -l * sin(theta));
   {
     stroke(200);
